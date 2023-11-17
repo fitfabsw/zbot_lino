cd586fbf75c749edfe6aa9f7a0bfbed2a51fd267
NickyTYWu <double.killout@gmail.com>
2023-11-17 15:08:58 +0800

    Added code examples for LED,BTT6030,LM5066I lib

--- linorobot2_hardware/firmware/lib/fit_debugmsg/debugmessage.cpp
+++ linorobot2_hardware/firmware/lib/fit_debugmsg/debugmessage.cpp
@@ -4,18 +4,22 @@
 void FITDBGbegin()
 {
 #ifdef FITDEBUGMESSAGE
+#if defined(ARDUINO_TEENSY41)
   Serial8.begin(115200);
+#endif
 #endif
 }
 
 void dbg_printf(const char *fmt, ...)
 {
 #ifdef FITDEBUGMESSAGE
+#if defined(ARDUINO_TEENSY41)
   va_list args;
   char buffer[128];
   va_start(args,fmt);
   vsprintf(buffer,fmt,args);
   va_end(args);
   Serial8.printf("%s",buffer);
+#endif
 #endif
 }
